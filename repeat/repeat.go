dc88a88580264ef25f00aa3f0663a2b9a4d9f40d
MukhammedZhumali <zhumalimuhammed@gmail.com>
2026-07-15 16:23:01 +0500

    repeating basics again

--- repeat/repeat.go
+++ repeat/repeat.go
@@ -4,5 +4,40 @@ import "fmt"
 
 
 func main() {
-	
+	x := 10
+
+	if( x < 5){
+		fmt.Println("x is less than 5")
+	}else {
+		fmt.Println("x is greater than or equal to 5")
+	}
+
+	if y:=10; y > 5 {
+		fmt.Println("y is greater than 5")
+	}
+
+	var num int
+	fmt.Println("Enter a number:")
+	fmt.Scanln(&num)
+	if(num < 0){
+		fmt.Println("The number is negative")
+	}else if (num == 0){
+		fmt.Println("The number is zero")
+	}else {
+		fmt.Println("The number is positive")
+	}
+
+	var age int 
+	fmt.Println("Enter your age:")
+	fmt.Scanln(&age)
+	if(age < 0){
+		panic("Age cannot be negative")
+	}
+	if ( age < 13){
+		fmt.Println("You are a child")
+	}else if ( age >= 13 && age < 18){
+		fmt.Println("You are a teenager")
+	}else{
+		fmt.Println("You are an adult")
+	}
 }
